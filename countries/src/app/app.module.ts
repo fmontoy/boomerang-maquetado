cb101cc53d83bb36d4df93a768a9e36514c5792d
Felipe Montoya Alvarez <felipemontoyaalvarez@Woloxs-MacBook-Pro-7.local>
2019-07-18 16:01:41 -0500

    Adding base project

--- countries/src/app/app.module.ts
+++ countries/src/app/app.module.ts
@@ -3,19 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { CountryCardComponent } from './components/country-card/country-card.component';
 import { CountryListComponent } from './screens/country-list/country-list.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CountryCardComponent,
     CountryListComponent,
-    NavbarComponent,
-    FooterComponent
   ],
   imports: [
     BrowserModule,
